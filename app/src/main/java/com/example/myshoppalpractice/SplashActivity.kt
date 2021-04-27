f8c7bbbb2abdab2057fe7e97545034a5e77f1131
Dxntoo <44705303+Dxntoo@users.noreply.github.com>
2021-04-27 15:57:09 +0800

    added splash screen

--- app/src/main/java/com/example/myshoppalpractice/SplashActivity.kt
+++ app/src/main/java/com/example/myshoppalpractice/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myshoppalpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Your Code
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish() // Call this when your activity is done and should be closed.
+
+        }, 3000)
     }
 }
